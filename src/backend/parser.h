babcaa66d788176cc0ad21c9f0564d6507f548ad
Jelinek-J <34974563+Jelinek-J@users.noreply.github.com>
2021-06-21 19:09:55 +0200

    Update parser.h

--- src/backend/parser.h
+++ src/backend/parser.h
@@ -16,7 +16,7 @@ namespace inspire {
       static std::string parse_id(const std::string& input) {
         std::ifstream stream(input);
         try {
-          if (common::string::ends_with(input, ".pdb")) {
+          if (common::string::ends_with(input, ".pdb") || common::string::ends_with(input, ".ent")) {
             return Pdb::parse_id(stream);
           } else if (common::string::ends_with(input, ".cif")) {
             return Mmcif::parse_id(stream);
@@ -34,7 +34,7 @@ namespace inspire {
       static bool contains_hetatoms(const std::string& input) {
         std::ifstream stream(input);
         try {
-          if (common::string::ends_with(input, ".pdb")) {
+          if (common::string::ends_with(input, ".pdb") || common::string::ends_with(input, ".ent")) {
             return Pdb::contains_hetatoms(stream);
           } else if (common::string::ends_with(input, ".cif")) {
             return Mmcif::contains_hetatoms(stream);
@@ -52,7 +52,7 @@ namespace inspire {
       static Protein parse_protein(std::string& input, BasicFilter* filter) {
         std::ifstream stream(input);
         try {
-          if (common::string::ends_with(input, ".pdb")) {
+          if (common::string::ends_with(input, ".pdb") || common::string::ends_with(input, ".ent")) {
             return Pdb::parse_protein(stream, filter);
           } else if (common::string::ends_with(input, ".cif")) {
             return Mmcif::parse_protein(stream, filter);
